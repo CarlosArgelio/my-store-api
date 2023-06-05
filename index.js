e21f41195cfeebe434efc4f510361ea3a5c8cf18
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-05 16:54:36 -0400

    feat: add route endpoint new-products and products

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hello World my server on express');
 })
 
+app.get('/new-route', (req, res) => {
+  res.send('Hello, i am new <strong>endpoint</strong>');
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+})
+
 app.listen(port, () => {
   console.log('My port' + port);
 });
