9e2542dd8ad49f69e54cdd54b1632eafd1877d5f
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-05 16:48:44 -0400

    installing express and config route '/'

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My APP')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World my server on express');
+})
+
+app.listen(port, () => {
+  console.log('My port' + port);
+});
