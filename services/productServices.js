63c313dd36fb16f1ce5b70f7b2db5fc281017d11
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-13 08:56:35 -0400

    install and configure sequalize

--- services/productServices.js
+++ services/productServices.js
@@ -1,15 +1,13 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom')
 
-const pool = require('../libs/postgresPool')
+const sequelize = require('../libs/sequelize')
 
 class ProductsServices {
 
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err));
   }
 
   generate() {
@@ -43,9 +41,10 @@ class ProductsServices {
 
   async find() {
     const query = 'SELECT * FROM task';
-    const rta = await this.pool.query(query);
 
-    return rta.rows
+    const [data] = await sequelize.query(query);
+
+    return data
   }
 
   async findOne(id) {
