a8cb8f300f4a6175099ddf98f03090ce7960c3e6
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-20 09:20:03 -0400

    customers configuration

---
+++ db/models/customerModel.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const { USER_TABLE } = require('./userModel');
+
+const CUSTOMER_TABLE = 'customers';
+
+const customerSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  lastName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'last_name'
+  },
+  phone: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW
+  },userId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'user_id',
+    references: {
+      model: USER_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  }
+}
+
+class Customer extends Model {
+
+  static associate(models) {
+    this.belongsTo(models.User, {as: 'user'});
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CUSTOMER_TABLE,
+      modelName: 'Customer',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { CUSTOMER_TABLE, customerSchema, Customer }
--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,11 @@
 const { User, UserSchema } = require('./userModel')
+const { Customer, customerSchema } = require('./customerModel')
 
 function setUpModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
+  Customer.init(customerSchema, Customer.config(sequelize));
+
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setUpModels;
